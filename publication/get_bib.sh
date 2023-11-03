69cb43aeeca3259787e1f443d6b650a95ca5a32f
Leslie-Lu <Leslie-Lu@users.noreply.github.com>
2023-11-03 05:07:58 +0000

    deploy: Leslie-Lu/WebsiteZhenLu@a3322e8c69dc0a017a9214a700bcf65a5192cb81

--- publication/get_bib.sh
+++ publication/get_bib.sh
@@ -4,3 +4,5 @@ RN-bibtex 413 > Lu-2023/cite.bib
 RN-bibtex 846 > Zou-2023/cite.bib
 RN-bibtex 832 > OPSCC-2023/cite.bib
 RN-bibtex 834 > Zhou-2023/cite.bib
+RN-bibtex 790 > Fu-2023/cite.bib
+RN-bibtex 796 > Tian-2023/cite.bib
