45bbbf149bf5a408c89e99be9fbab5b52f123c19
Leslie-Lu <Leslie-Lu@users.noreply.github.com>
2024-01-09 09:37:12 +0000

    deploy: Leslie-Lu/WebsiteZhenLu@2af65eba3a8256d18978894a66fa6251e3dc9607

--- publication/get_bib.sh
+++ publication/get_bib.sh
@@ -1,9 +1,13 @@
 cat t | tr "," "\n-" | awk '{print "-"$0}'
 
-RN-bibtex 413 > Lu-2023/cite.bib
-RN-bibtex 846 > Zou-2023/cite.bib
-RN-bibtex 832 > OPSCC-2023/cite.bib
-RN-bibtex 834 > Zhou-2023/cite.bib
-RN-bibtex 790 > Fu-2023/cite.bib
-RN-bibtex 796 > Tian-2023/cite.bib
-RN-bibtex 833 > Wang-2023/cite.bib
+RN-bibtex 413 > Lu-COVID19-2023/cite.bib
+RN-bibtex 846 > Lu-neurosyphilis-diagnosis-2023/cite.bib
+RN-bibtex 832 > Lu-OPSCC-2023/cite.bib
+RN-bibtex 834 > Zhou-HPV-MSM-2023/cite.bib
+RN-bibtex 790 > Fu-Kaposi-sarcoma-2023/cite.bib
+RN-bibtex 796 > Tian-PrEP-2023/cite.bib
+RN-bibtex 833 > Wang-SWELL-2023/cite.bib
+
+# 2024
+RN-bibtex 1047 > Yang-cardivascular-desease-2023/cite.bib
+RN-bibtex 1044 > Fu-HIV-STI-2023/cite.bib
